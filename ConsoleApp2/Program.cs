92bae6e361df52de0b96c7b52b53a91269fef8b7
NikkkkaaBabych <magictime.jkrowling@gmail.com>
2020-03-09 15:33:00 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, c, x, y;
+
+            Console.WriteLine("Введите c=");
+            c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите a=");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите x=");
+            x = Convert.ToDouble(Console.ReadLine());
+            if ((2 * Math.Sin(Math.PI / 3 + x) == 0) || (x <= 0))
+            {
+                Console.WriteLine("Невозможно выполнить операции. Введите другие значения");
+            }
+            else
+            {
+                if (c * x - a >= 0)
+                {
+                    y = Math.Pow((c * x - a), 1.0 / 3) + (Math.Log(x)) / (2 * Math.Sin(Math.PI / 3 + x));
+                }
+                else
+                {
+                    y = -Math.Pow(-(c * x - a), 1.0 / 3) + (Math.Log(x)) / (2 * Math.Sin(Math.PI / 3 + x));
+                }
+                Console.WriteLine("y= {0}", y);
+            }
+            Console.ReadKey();
+        }
+    }
+}
